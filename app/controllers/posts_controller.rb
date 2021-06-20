13b021e3d0771822b39fe2696e377115473021fe
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-20 23:10:30 +0900

    軽微な修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @post = Post.order(id: :asc)
+    @posts = Post.order(id: :asc)
   end
 
   def new
