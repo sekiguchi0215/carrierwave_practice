f8246217fb41d9bcfe2f73c46a67a91d3cee6565
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-21 16:18:54 +0900

    画像サイズが変更されるように設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -20,6 +20,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
     "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
   end
+
+  process resize_to_limit: [200, 300]
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
